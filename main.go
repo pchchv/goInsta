38c5c7aae5c1d88ec60aac6f29d8b105c565ea0b
pchchv <ipchchv@gmail.com>
2022-09-16 20:19:37 +0300

    Implement data retrieval from the environment

--- main.go
+++ main.go
@@ -3,13 +3,31 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 	"path/filepath"
 
 	"github.com/gedex/go-instagram/instagram"
+	"github.com/joho/godotenv"
 )
 
 var instaName = flag.String("n", "", "Instangram user name such as: 'pchchv'")
 var client *instagram.Client
+
+func init() {
+	// Load values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Panic("No .env file found")
+	}
+}
+
+func getEnvValue(v string) string {
+	// Getting a value. Outputs a panic if the value is missing.
+	value, exist := os.LookupEnv(v)
+	if !exist {
+		log.Panicf("Value %v does not exist", v)
+	}
+	return value
+}
 
 func main() {
 	flag.Parse()
