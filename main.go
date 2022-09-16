5c95c2e4613de95ad7da5230e9ba02393ebf676d
pchchv <ipchchv@gmail.com>
2022-09-16 20:14:18 +0300

    Set the folder for saving photos

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"path/filepath"
 )
 
 var instaName = flag.String("n", "", "Instangram user name such as: 'pchchv'")
@@ -13,4 +15,10 @@ func main() {
 		log.Fatal("You need to input -n=name")
 	}
 	input := *instaName
+	// Set the folder for saving photos
+	baseDir, err := filepath.Abs("../photo")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(baseDir)
 }
